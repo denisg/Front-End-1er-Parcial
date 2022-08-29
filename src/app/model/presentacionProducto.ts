9e5380429a4bd95336af8cc9e31d084d1330aee0
heokim <andresssshk@gmail.com>
2022-08-28 20:31:44 -0400

    modelos de productos

---
+++ src/app/model/presentacionProducto.ts
@@ -0,0 +1,7 @@
+import { ExistenciaProducto } from './existenciaProducto';
+
+export class PresentacionProducto {
+  idPresentacionProducto!: number;
+  existenciaProducto!: ExistenciaProducto;
+  nombre!: string;
+}
