54a4a4bd930f73d7693c7c64b7f76886a543d429
heokim <andresssshk@gmail.com>
2022-08-28 20:16:24 -0400

    correcion de bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,27 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import {PaisComponent} from "./pais/pais.component";//se importa el componente
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { PaisComponent } from './pais/pais.component';
+import { LoginComponent } from './login/login.component';
 
 //se rutea los componentes aqui, que es el componente principal
 const routes: Routes = [
   {
-    path: "",
+    path: '',
     component: HomeComponent,
   },
   {
     path: 'pais',
-    component: PaisComponent
-  },{
-    path: "login",
-    component: LoginComponent
+    component: PaisComponent,
   },
-
-
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { PaisComponent } from './pais/pais.component';
-import {ServicepaisService} from "./service/servicepais.service";
 import { HomeComponent } from './home/home.component';
-import {HttpClient, HttpClientModule} from "@angular/common/http";
+import { HttpClientModule } from '@angular/common/http';
 import { ServicepersonaService } from './service/servicepersona.service';
-import { FormsModule } from '@angular/forms';
-
+import { NgxPaginationModule } from 'ngx-pagination';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PaisComponent } from './pais/pais.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    PaisComponent,
-    HomeComponent
-  ],
+  declarations: [AppComponent, LoginComponent, PaisComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    NgxPaginationModule,
+    NgbModule,
   ],
-  providers: [
-    ServicepersonaService
-  ],
-  bootstrap: [AppComponent]
+  providers: [ServicepersonaService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
