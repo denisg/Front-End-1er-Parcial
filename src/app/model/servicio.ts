2c22648afc3eabfe4dfb0ea7f0f6cfd9eb6f8778
heokim <andresssshk@gmail.com>
2022-08-28 20:37:05 -0400

    modelo de detalles y servicios

---
+++ src/app/model/servicio.ts
@@ -0,0 +1,29 @@
+import { Ficha } from './fichas';
+import { PresentacionProducto } from './presentacionProducto';
+
+export class Servicio {
+  idServicio!: number;
+  //flagEstado: R,
+  fechaHora!: string;
+  presupuesto!: number;
+  idFichaClinica!: Ficha;
+  observacion!: string;
+  //listaDetalles!: T[];
+}
+
+export class ServicioPostBody {
+  idFichaClinica!: Partial<Ficha>;
+  observacion!: string;
+}
+
+export class DetalleS {
+  idServicioDetalle!: number;
+  idPresentacionProducto!: PresentacionProducto;
+  idServicio!: Servicio;
+}
+
+export class DetallePostBody {
+  cantidad!: number;
+  idPresentacionProducto!: Partial<PresentacionProducto>;
+  idServicio!: Partial<Servicio>;
+}
