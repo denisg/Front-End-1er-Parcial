116102346e36ccbd4378c27549974200aef381dd
heokim <andresssshk@gmail.com>
2022-08-29 10:03:23 -0400

    agregado rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,16 @@ import { ServicepersonaService } from './service/servicepersona.service';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { PaisComponent } from './pais/pais.component';
-import { FichaComponent } from './ficha/ficha.component';
-import { NuevafichaComponent } from './ficha/nuevaficha/nuevaficha.component';
+// import { FichaComponent } from './ficha/ficha.component';
+// import { NuevafichaComponent } from './ficha/nuevaficha/nuevaficha.component';
 import { BuscarclienteComponent } from './buscarcliente/buscarcliente.component';
 import { BuscarempleadoComponent } from './buscarempleado/buscarempleado.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, PaisComponent, HomeComponent, FichaComponent, NuevafichaComponent, BuscarempleadoComponent,
+  declarations: [AppComponent, LoginComponent, PaisComponent, HomeComponent,
+    //  FichaComponent,
+    //  NuevafichaComponent,
+    BuscarempleadoComponent,
     BuscarclienteComponent,],
   imports: [
     BrowserModule,
