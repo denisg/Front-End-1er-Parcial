7b7e491b2d363f12ff91a07cb62c5d6e8a38bccc
denisg <denismaldonado69@gmail.com>
2022-08-31 20:44:53 -0400

    agregado de directorios de 'reserva'

---
+++ src/app/reserva/modificar-reserva/modificar-reserva.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modificar-reserva',
+  templateUrl: './modificar-reserva.component.html',
+  styleUrls: ['./modificar-reserva.component.css']
+})
+export class ModificarReservaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/reserva/nueva-reserva/nueva-reserva.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nueva-reserva',
+  templateUrl: './nueva-reserva.component.html',
+  styleUrls: ['./nueva-reserva.component.css']
+})
+export class NuevaReservaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
